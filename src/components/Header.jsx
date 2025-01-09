6e33da045fbd3f750c5f96abdb5620e29f06d45c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-09 11:21:44 +0530

    feat: enhance layout and styling of Header component with logo and description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import githubIcon from "../assets/img/github.svg";
+import hueHunterlogo from "../assets/img/huehunter-logo.png";
 
 export const Header = () => {
   return (
-    <header className="grid place-items-center text-2xl py-5 text-white">
-      <div className="flex justify-between w-11/12">
-        <div className="font-bold text-3xl">Image colour picker</div>
+    <header className="grid py-5 text-2xl text-white place-items-center">
+      <div className="flex items-center justify-between w-11/12">
+        <div className="flex items-center">
+          <img className="h-16" src={hueHunterlogo} alt="HueHunter" />
+          <div className="">
+            <div className="text-3xl font-bold">HueHunter</div>
+            <div className="hidden text-sm md:block">
+              A sleek and intuitive tool to extract and explore colors from any
+              image effortlessly.
+            </div>
+          </div>
+        </div>
         <div className="">
           <a href="https://github.com/aumirza/image-color-picker">
             <img
-              className="h-8 w-8 brightness-100 invert"
+              className="w-10 h-10 brightness-100 invert"
               src={githubIcon}
               alt=""
             />
