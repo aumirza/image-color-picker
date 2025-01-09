2fbc6155054e6b26738b51062a46b87bfbf48c20
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-09 11:17:01 +0530

    feat: enhance layout and styling of Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="flex justify-center text-white text-xl">
+    <div className="z-10 flex justify-center py-3 text-xl text-white bg-pink-800">
       <div className="">
         Made with ❤️ love by
         <a className="underline" href="https://ahmadullah.in">
